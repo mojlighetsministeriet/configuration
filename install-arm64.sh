2a32a824113cfe4aceb4b99865ddfd2ea4bb184b
Fredrik Söderström <fredrik.soderstrom@cambio.se>
2017-11-30 23:24:19 +0100

    Change dash to dot

--- install-arm64.sh
+++ install-arm64.sh
@@ -4,6 +4,6 @@ docker swarm init
 
 docker network create -d overlay proxy
 
-docker stack deploy -c docker-compose.traefik-arm64.yml traefik
-docker stack deploy -c docker-compose.persistence-arm-64.yml persistence
-docker stack deploy -c docker-compose.services-arm64.yml services
+docker stack deploy -c docker-compose.traefik.arm64.yml traefik
+docker stack deploy -c docker-compose.persistence.arm-64.yml persistence
+docker stack deploy -c docker-compose.services.arm64.yml services
