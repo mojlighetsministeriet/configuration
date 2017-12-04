82f7112be1b3666531f69be70d30260baf4e7b77
Fredrik Söderström <fredrik.soderstrom@cambio.se>
2017-12-04 09:51:34 +0100

    Update configuration

--- install-arm64.sh
+++ install-arm64.sh
@@ -5,5 +5,5 @@ docker swarm init
 docker network create -d overlay proxy
 
 docker stack deploy -c docker-compose.traefik.arm64.yml traefik
-docker stack deploy -c docker-compose.persistence.arm-64.yml persistence
+docker stack deploy -c docker-compose.persistence.arm64.yml persistence
 docker stack deploy -c docker-compose.services.arm64.yml services
