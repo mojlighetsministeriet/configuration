f0a68b79f0e5e95019671110c34177df886118c1
Fredrik Söderström <fredrik.soderstrom@cambio.se>
2017-12-14 11:25:43 +0100

    Remove arch/os checks for now

--- install-arm64.sh
+++ install-arm64.sh
@@ -4,6 +4,6 @@ docker swarm init
 
 docker network create -d overlay proxy
 
-docker stack deploy -c docker-compose.traefik.arm64.yml traefik
-docker stack deploy -c docker-compose.persistence.arm64.yml persistence
-docker stack deploy -c docker-compose.services.arm64.yml services
+docker stack deploy --resolve-image=never -c docker-compose.traefik.arm64.yml traefik
+docker stack deploy --resolve-image=never -c docker-compose.persistence.arm64.yml persistence
+docker stack deploy --resolve-image=never -c docker-compose.services.arm64.yml services
